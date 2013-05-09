5459903805911a4e31e71c25c230bfbe2b2d9e15
Love Nyberg <love.nyberg@viaplay.com>
2013-05-09 18:55:25 +0200

    Removed init script as it was made for Jenkins Master. Now it will only start as java process.a

--- setup.sh
+++ setup.sh
@@ -1,9 +1,30 @@
-sudo mv jenkins-slave /etc/default/
-sudo mv jenkins-slave.init /etc/init.d/jenkins-slave
-ls -la /etc/init.d/jenkins-slave 
-chmod 770 /etc/init.d/jenkins-slave 
-chown root:root /etc/init.d/jenkins-slave 
-sudo chown root:root /etc/init.d/jenkins-slave 
+#!/bin/bash
+usage() {
+	echo "usage: sudo setup.sh <node-name> <jenkins-master-fqdn>"
+}
+
+dependency() {
+	echo "dependency: Require java but it's not installed. Aborting"
+}
+
+if [ $# -ne 2 ]; then
+	usage
+	exit 1
+fi
+
+command -v java >/dev/null 2>&1 || { dependency; exit 1; }
+
+nodeName=$1
+fqdn=$2
+
+jarUrl="http://$fqdn/jnlpJars/slave.jar"
+nodeUrl="http://$fqdn/computer/$nodeName/slave-agent.jnlp"
+
+sed -i 's/nodeUrl/$nodeUrl/g' jenkins-slave
+
 mkdir /opt/jenkins-slave/
 cd /opt/jenkins-slave/
-curl -O https://test.viaplay.tv/jenkins/computer/SLAVENAME/slave-agent.jnlp
+
+curl -O $nodeUrl
+curl -O $jarUrl
+nohup java -jar slave.jar -jnlpUrl $nodeUrl
